2c97652330c9c1f3c7057d745c2cbe7663e0de07
Michiel de Jong <michiel@pondersource.com>
2024-06-27 14:03:39 +0200

    Log POST bodies

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const { createServer } = require('http');
 const port = process.env.PORT || 3000;
 createServer((req, res) => {
+  let body = '';
+  req.on('data', function(data) {
+    body += data;
+
+    // Too much POST data, kill the connection!
+    // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+    if (body.length > 1e6) {
+      request.connection.destroy();
+    }
+  });
+  req.on('end', function() {
+    console.log(body);
+  });
+
   res.end('https://taskifier.heroku.com works');
 }).listen(port);
 console.log('listening on port', port);
